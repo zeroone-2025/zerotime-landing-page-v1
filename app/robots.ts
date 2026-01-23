61a3196ca1b443285eec9bf5ac3e861daf5e35be
hcw889 <hcw88928@naver.com>
2026-01-23 18:49:52 +0900

    fix:sitemap_Domain_fix

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://zerotime.kr/sitemap.xml",
+    sitemap: "https://home.zerotime.kr/sitemap.xml",
   };
 }
